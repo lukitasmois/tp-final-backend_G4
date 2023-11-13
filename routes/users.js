8da8fb043f1f523c043a0b3fd82a7a28d382a6ca
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2023-11-13 19:36:44 -0300

    agrego el registro de usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const dataUser = require('../data/users.js');
+const { body, validationResult } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/register', async(req,res) =>{
+  try {
+    const newUser = req.body;
+    const result = await dataUser.addUser(newUser);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+})
 
 module.exports = router;
