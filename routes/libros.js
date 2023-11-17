ee4ffccffa8edc931f87c2d2294be85cb6de81ca
eileenss <eileenssanchez97@gmail.com>
2023-11-16 23:41:09 -0300

    crud libros: addLibro, deleteLibro, updateLibro sin terminar

--- routes/libros.js
+++ routes/libros.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const controller = require("../controllers/libros");
+const data = require('../data/libros');
 
 /* GET libros listing. */
 router.get("/", async function (req, res, next) {
@@ -9,6 +10,39 @@ router.get("/", async function (req, res, next) {
 
 router.get("/:id", async (req, res) => {
   res.json(await controller.getLibro(req.params.id));
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const newLibro = req.body;
+    const result = await data.addLibro(newLibro);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await data.deleteLibro(id);
+    res.send(result)
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await data.updateLibro(id);
+    res.send(result)
+  } catch (error) { 
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
